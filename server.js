2cdeff225e26c86d2ddb0f59812c04771245e2b3
AndreiMorariu <andreimorariu4140@gmail.com>
2024-05-27 22:08:24 +0300

    commit

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 require("dotenv").config();
 app.use(express.json());
@@ -8,7 +9,7 @@ const usersRoute = require("./routes/usersRoute");
 const examsRoute = require("./routes/examsRoute");
 const resportsRoute = require("./routes/reportsRoute");
 
-
+app.use(cors());
 app.use("/api/users", usersRoute);
 app.use("/api/exams", examsRoute);
 app.use("/api/reports", resportsRoute);
@@ -17,13 +18,12 @@ const port = process.env.PORT || 5000;
 const path = require("path");
 __dirname = path.resolve();
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client" , "build")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });   
-} 
-
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "client" , "build")));
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+//   });
+// }
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
